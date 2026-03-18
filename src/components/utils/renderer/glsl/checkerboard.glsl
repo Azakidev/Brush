5b339519fcba08e427c4a67eb6ba78cc00439455
Azakidev <zazaguichi@outlook.com>
2026-03-18 23:44:32 +0100

    Six seven

--- src/components/utils/renderer/glsl/checkerboard.glsl
+++ src/components/utils/renderer/glsl/checkerboard.glsl
@@ -14,8 +14,8 @@ void main() {
 
     float pattern = mod(pos.x + pos.y, 2.0);
 
-    vec3 color1 = vec3(0.8);
-    vec3 color2 = vec3(0.9);
+    vec3 color1 = vec3(0.6);
+    vec3 color2 = vec3(0.7);
 
     vec3 final_color = mix(color1, color2, pattern);
     color = vec4(final_color, 1.0);
